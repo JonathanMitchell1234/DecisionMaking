1927171f714e4d9640d79564b8a7770497ed08f5
Jonathan Mitchell <jonmitchell1234@gmail.com>
2021-10-18 18:09:47 -0400

    DecisionMakingExercise

---
+++ decisionmaking.js
@@ -0,0 +1,56 @@
+// DECISION MAKING EXERCISE
+
+
+let randomNum = Math.floor(Math.random() * 5 + 1);
+
+console.log(randomNum);
+
+
+if (randomNum >= 4) {
+  console.log("Greater than or equal to 4");
+} else if (randomNum === 2 || randomNum === 3) {
+  console.log("Equal to 2 or 3");
+} else {
+  console.log("Equal to 1");
+}
+
+
+
+if (randomNum >= 4) {
+
+  console.log("Greater than or equal to 4");
+} else if (randomNum >= 2) {
+
+  console.log("Equal to 2 or 3");
+} else {
+  console.log("Equal to 1");
+}
+
+
+if (randomNum !== 3){
+  console.log("Not equal to 3");
+}
+
+
+
+if (randomNum === 1 || randomNum === 2 || randomNum === 4 || randomNum === 5) {
+  console.log("Not equal to 3");
+}
+
+
+if (randomNum !== 3 && randomNum !== 5){
+  console.log("NOT equal to 3 AND not equal to 5");
+}
+
+
+
+if (randomNum === 1 || randomNum === 2 || randomNum === 4) {
+console.log(randomNum);
+  console.log("NOT equal to 3 AND not equal to 5");
+}
+
+
+if (randomNum === 2 || randomNum === 4) {
+  console.log("Equal to 2 or equal to 4");
+}
+
